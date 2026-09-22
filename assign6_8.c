9a59549cc6232a770e4383a386d806fa80e697ef
akriti-590037240-B5 <akriti.davas@gmail.com>
2026-09-22 19:23:45 +0530

    assign 6 question8

---
+++ assign6_8.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+
+int main()
+{
+    int n,i,j;
+    double x, power,factorial,term,sum=0.0;
+
+    printf("Enter the value of x: ");
+    scanf("%lf",&x);
+
+    printf("Enter the number of terms:");
+    scanf("%d",&n);
+
+    if(n <= 0)
+    {
+        printf("Number of terms must be positive");
+    }
+    else
+    {
+        for(i=1;i<=n;i++)
+        {
+            power = 1.0;
+            factorial = 1.0;
+
+            for(j=1;j<=i;j++)
+            {
+                power = power*x;
+            }
+
+            for(j=1;j<=i;j++)
+            {
+                factorial = factorial*j;
+            }
+
+            term = power/factorial;
+
+            if(i % 2 == 0)
+            sum = sum - term;
+            else
+            sum = sum+term;
+        }   
+        printf("The sum of the series is = %4lf", sum);
+   }
+   return 0;
+    }
